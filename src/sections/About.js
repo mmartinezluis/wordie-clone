9dd3fad7034d7c92897075ab9319950b027c0033
Luis Martinez <luis_mmartinez@live.com>
2022-12-31 17:26:20 -0500

    update readme of about page and help page

--- src/sections/About.js
+++ src/sections/About.js
@@ -1,36 +1,80 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-    return(
-        <div className='about'>
-            <h2>About</h2>
-            <p>I created <strong>Wordle Clone</strong> with the goal of implementing <strong>Data Structures and Algorithms</strong> (WCDSA) to real-world scenarios,
-                and as a means of writing efficient code in practice. Wordle Clone utilizes a combination of a matrix and 
-                queue data structures and a counter variable to access user input in constant time at any given 
-                time during the game. This combination also allows to more easily implement special features, such as
-                specific-cell editing.
-            </p>
-            <p>As simple as it may seem, Wordle Clone came with many interesting challenges that helped me 
-                become a better problem solver and developer.
-            </p>
-            <p>Wordle Clone is playable in desktop and mobile. </p>
+  return (
+    <div className="about">
+      <h2>About</h2>
+      <p>
+        I created <strong>Wordie Clone</strong> with the goal of implementing{" "}
+        <strong>Data Structures and Algorithms</strong> (WCDSA) to real-world
+        scenarios, and as a means of writing efficient code in practice. Wordie
+        Clone utilizes a combination of a matrix and queue data structures and a
+        pointer to access user input in constant time at any given time during
+        the game. This combination also allows to more easily implement special
+        features, such as specific-cell editing.
+      </p>
+      <p>
+        As simple as it may seem, Wordie Clone came with many interesting
+        challenges that helped me become a better problem solver and developer.
+      </p>
+      <p>Wordie Clone is playable in desktop and mobile. </p>
 
-            <h3>Blog Posts</h3>
-            <p>You can find a discussion of Wordle Clone DSA's <strong><a href='https://devblog.dev/luismartinez/designing-wordle-wordle-clone-dsa-part-i-data-architecture-6defbeea' target="_blank" rel='noreferrer'>data architecture</a></strong> and <strong><a href='https://devblog.dev/luismartinez/designing-wordle-wordle-clone-dsa-part-ii-data-implementation-1d370b6514' target="_blank" rel='noreferrer'>data implementation</a></strong> on this line.
-            </p>
+      <h3>Blog Posts</h3>
+      <p>
+        You can find a discussion of Wordie Clone DSA's{" "}
+        <strong>
+          <a
+            href="https://devblog.dev/luismartinez/designing-wordle-wordle-clone-dsa-part-i-data-architecture-6defbeea"
+            target="_blank"
+            rel="noreferrer"
+          >
+            data architecture
+          </a>
+        </strong>{" "}
+        and{" "}
+        <strong>
+          <a
+            href="https://devblog.dev/luismartinez/designing-wordle-wordle-clone-dsa-part-ii-data-implementation-1d370b6514"
+            target="_blank"
+            rel="noreferrer"
+          >
+            data implementation
+          </a>
+        </strong>{" "}
+        on this line.
+      </p>
 
-            <h3>Performance Tests</h3>
-            <p>You can play with some live <strong><a href='https://k2cc2k.csb.app/' target="_blank" rel='noreferrer'>performance tests</a></strong> for Wordle Clone DSA on this line.</p>
+      <h3>Performance Tests</h3>
+      <p>
+        You can play with some live{" "}
+        <strong>
+          <a href="https://k2cc2k.csb.app/" target="_blank" rel="noreferrer">
+            performance tests
+          </a>
+        </strong>{" "}
+        for Wordie Clone DSA on this line.
+      </p>
 
-            <h3>Code</h3>
-            <p>You can find the full code for Wordle Clone in its <strong><a href='https://github.com/mmartinezluis/wordle-clone' target="_blank" rel='noreferrer'>github respository</a></strong>.
-            </p>
+      <h3>Code</h3>
+      <p>
+        You can find the full code for Wordie Clone in its{" "}
+        <strong>
+          <a
+            href="https://github.com/mmartinezluis/wordle-clone"
+            target="_blank"
+            rel="noreferrer"
+          >
+            github respository
+          </a>
+        </strong>
+        .
+      </p>
 
-            <p>Happy coding!!!</p>
-            <p>Luis Martinez</p>
-
-        </div>
-    )
-}
+      <p>
+        <em>{"Happy coding!!! \nLuis Martinez"}</em>
+      </p>
+    </div>
+  );
+};
 
 export default About;
--- src/sections/Help.js
+++ src/sections/Help.js
@@ -1,99 +1,144 @@
-import React from 'react';
+import React from "react";
 
 const examples = [
-    ['S', 'T', 'A', 'C', 'K'],
-    ['P', 'H', 'A', 'S', 'E'],
-    ['G', 'R', 'A', 'P', 'E'],
-    ['Q', 'U', 'O', 'U', 'E'],
-    ['R', '-', 'S', '-', 'T']
-]
+  ["S", "T", "A", "C", "K"],
+  ["P", "H", "A", "S", "E"],
+  ["G", "R", "A", "P", "E"],
+  ["Q", "U", "O", "U", "E"],
+  ["R", "-", "S", "-", "T"],
+];
 
 const assertion_matrix = [
-    ['right', '', '', '', ''],
-    ['', 'close', '', '', ''],
-    ['', '', '', '', 'missed'],
-    ['', '', '', '', ''],
-    ['', '', '', '', '']
-]
+  ["right", "", "", "", ""],
+  ["", "close", "", "", ""],
+  ["", "", "", "", "missed"],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+];
 
 const Help = () => {
-    return (
-        <div className='help'>
-            <h2>How to Play</h2>
-            <p>The goal of the game is to guess the five-characters word in at most 6 attempts.
-               After every attempt, each of the letters in the attempted row will be color-coded depending on 
-               whether the letter is not in the word at all (gray cells), it's in the word, but not in the
-               correct position (yellow cells), or it's in the word and in the correct position (green cells). Examples:
-            </p>
-            <div className='board'>
-                <div className='row'>
-                    {new Array(1).fill(0).map((r, ri) => new Array(5).fill(0).map((c, ci) => {
-                        return <div className={`cell ${assertion_matrix[0][ci]}`} key={ci}>
-                                    {examples[0][ci]}
-                                </div>
-                    }))}
+  return (
+    <div className="help">
+      <h2>How to Play</h2>
+      <p>
+        The goal of the game is to guess the five-characters word in at most 6
+        attempts. After every attempt, each of the letters in the attempted row
+        will be color-coded depending on whether the letter is not in the word
+        at all (gray cells), it's in the word, but not in the correct position
+        (yellow cells), or it's in the word and in the correct position (green
+        cells). Examples:
+      </p>
+      <div className="board">
+        <div className="row">
+          {new Array(1).fill(0).map((r, ri) =>
+            new Array(5).fill(0).map((c, ci) => {
+              return (
+                <div className={`cell ${assertion_matrix[0][ci]}`} key={ci}>
+                  {examples[0][ci]}
                 </div>
-            </div>
-            <div className='examples'><span><strong>S</strong> is in the word and in the correct position</span></div>
-            <div className='board'>
-                <div className='row'>
-                    {new Array(1).fill(0).map((r, ri) => new Array(5).fill(0).map((c, ci) => {
-                        return <div className={`cell ${assertion_matrix[1][ci]}`} key={ci}>
-                                    {examples[1][ci]}
-                                </div>
-                    }))}
-                </div>
-            </div>
-            <div className='examples'><span><strong>H</strong> is in the word, but in the wrong position</span></div>
-            <div className='board'>
-                <div className='row'>
-                    {new Array(1).fill(0).map((r, ri) => new Array(5).fill(0).map((c, ci) => {
-                        return <div className={`cell ${assertion_matrix[2][ci]}`} key={ci}>
-                                    {examples[2][ci]}
-                                </div>
-                    }))}
-                </div>
-            </div>
-            <div className='examples'><span><strong>E</strong> is not in the word</span></div>
-            <br/>
-
-            <h3>Editing Cells</h3>
-            <p>If you click on any nonempty cell within the current row, that cell becomes editable, 
-                and it will glow:
-            </p>
-            <div className='board'>
-                <div className='row'>
-                    {new Array(1).fill(0).map((r, ri) => new Array(5).fill(0).map((c, ci) => {
-                        return <div className={ `cell  ${ci === 2 ? 'glow' : ''} `} key={ci}>
-                                    {examples[3][ci]}
-                                </div>
-                    }))}
-                </div>
-            </div>
-            <br/><br/>
-
-            <h3>Placeholders</h3>
-            <p>Pressing the space key in your device or on the page's keyboard will insert a dash 
-                as a letter placeholder for the cell. Dashes will later need to be replaced for your 
-                word to be processed.
-            </p>
-            <div className='board'>
-                <div className='row'>
-                    {new Array(1).fill(0).map((r, ri) => new Array(5).fill(0).map((c, ci) => {
-                        return <div className='cell' key={ci}>
-                                    {examples[4][ci]}
-                                </div>
-                    }))}
-                </div>
-            </div>
-            <br/><br/>
-
-            <h3>Re-Starting the Game</h3>
-            <p>You can start a new game at any time by pressing the <em>New Game</em> button in the header section.</p>
-            <p>Can you guess the words for PHASE, GRAPE, and QUOUE from the above examples? (Hint: they come from data structures and algorithms topics)</p>
-            <p>Have fun with Wordle Clone!!!</p>
+              );
+            })
+          )}
         </div>
-    )
-}
+      </div>
+      <div className="examples">
+        <span>
+          <strong>S</strong> is in the word and in the correct position
+        </span>
+      </div>
+      <div className="board">
+        <div className="row">
+          {new Array(1).fill(0).map((r, ri) =>
+            new Array(5).fill(0).map((c, ci) => {
+              return (
+                <div className={`cell ${assertion_matrix[1][ci]}`} key={ci}>
+                  {examples[1][ci]}
+                </div>
+              );
+            })
+          )}
+        </div>
+      </div>
+      <div className="examples">
+        <span>
+          <strong>H</strong> is in the word, but in the wrong position
+        </span>
+      </div>
+      <div className="board">
+        <div className="row">
+          {new Array(1).fill(0).map((r, ri) =>
+            new Array(5).fill(0).map((c, ci) => {
+              return (
+                <div className={`cell ${assertion_matrix[2][ci]}`} key={ci}>
+                  {examples[2][ci]}
+                </div>
+              );
+            })
+          )}
+        </div>
+      </div>
+      <div className="examples">
+        <span>
+          <strong>E</strong> is not in the word
+        </span>
+      </div>
+      <br />
+
+      <h3>Editing Cells</h3>
+      <p>
+        If you click on any nonempty cell within the current row, that cell
+        becomes editable, and it will glow:
+      </p>
+      <div className="board">
+        <div className="row">
+          {new Array(1).fill(0).map((r, ri) =>
+            new Array(5).fill(0).map((c, ci) => {
+              return (
+                <div className={`cell  ${ci === 2 ? "glow" : ""} `} key={ci}>
+                  {examples[3][ci]}
+                </div>
+              );
+            })
+          )}
+        </div>
+      </div>
+      <br />
+      <br />
+
+      <h3>Placeholders</h3>
+      <p>
+        Pressing the space key in your device or on the page's keyboard will
+        insert a dash as a letter placeholder for the cell. Dashes will later
+        need to be replaced for your word to be processed.
+      </p>
+      <div className="board">
+        <div className="row">
+          {new Array(1).fill(0).map((r, ri) =>
+            new Array(5).fill(0).map((c, ci) => {
+              return (
+                <div className="cell" key={ci}>
+                  {examples[4][ci]}
+                </div>
+              );
+            })
+          )}
+        </div>
+      </div>
+      <br />
+      <br />
+
+      <h3>Re-Starting the Game</h3>
+      <p>
+        You can start a new game at any time by pressing the <em>New Game</em>{" "}
+        button in the header section.
+      </p>
+      <p>
+        Can you guess the words for PHASE, GRAPE, and QUOUE from the above
+        examples? (Hint: they come from data structures and algorithms topics)
+      </p>
+      <p>Have fun with Wordie Clone!!!</p>
+    </div>
+  );
+};
 
 export default Help;
